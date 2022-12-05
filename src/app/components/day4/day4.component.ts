5917ae94fb0342749aade877ed965b25a27f7b69
amoeller <alexander.moeller@adesso.de>
2022-12-05 19:42:33 +0100

    solved Day4

--- src/app/components/day4/day4.component.ts
+++ src/app/components/day4/day4.component.ts
@@ -1,4 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {realData_4, testData_4} from "./data_4";
+
+class ElfWithRange {
+  constructor(public startIndex: number, public endIndex: number) {
+  }
+}
+
+class ElfPair {
+  constructor(public firstElf: ElfWithRange, public secondElf: ElfWithRange) {
+  }
+}
 
 @Component({
   selector: 'app-day4',
@@ -6,10 +17,61 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day4.component.scss']
 })
 export class Day4Component implements OnInit {
-
-  constructor() { }
+  loesung_1 = 0;
+  losesung_2 = 0;
 
   ngOnInit(): void {
+    const parsedData = this.parser(realData_4);
+    console.log(parsedData)
+    this.loesung_1 = parsedData.map(pair => this.doesOneElfAreaIncludeTheOther(pair)).reduce((a: number, b) => {
+      return a + (b ? 1 : 0);
+    }, 0);
+
+    this.losesung_2 = parsedData.map(pair => this.isThereOverlap(pair)).reduce((a: number, b) => {
+      return a + (b ? 1 : 0);
+    }, 0);
+
   }
+
+  doesOneElfAreaIncludeTheOther(pair: ElfPair) {
+    const oneIncludesTwo = pair.firstElf.startIndex <= pair.secondElf.startIndex && pair.firstElf.endIndex >= pair.secondElf.endIndex
+    const twoIncludesOne = pair.secondElf.startIndex <= pair.firstElf.startIndex && pair.secondElf.endIndex >= pair.firstElf.endIndex
+    return oneIncludesTwo || twoIncludesOne;
+  }
+
+  isThereOverlap(pair: ElfPair): boolean {
+    const oneIncludesTheOther = this.doesOneElfAreaIncludeTheOther(pair);
+    if (oneIncludesTheOther) {
+      return true;
+    }
+
+    let thereIsOverlap = false;
+    if (pair.firstElf.startIndex === pair.secondElf.startIndex || pair.firstElf.endIndex === pair.secondElf.endIndex) {
+      // starten oder enden gemeinsam
+      thereIsOverlap = true;
+    }
+
+    if (pair.firstElf.startIndex < pair.secondElf.startIndex) {
+      // 1 startet vor 2
+      thereIsOverlap = pair.firstElf.endIndex >= pair.secondElf.startIndex;
+    }
+
+    if (pair.firstElf.startIndex > pair.secondElf.startIndex) {
+      // 1 startet nach 2
+      thereIsOverlap = pair.firstElf.startIndex <= pair.secondElf.endIndex;
+    }
+    return thereIsOverlap;
+  }
+
+  parser(data: string): ElfPair[] {
+    const pairStrings: string[] = data.split("\n");
+    return pairStrings
+      .map(pairstr => pairstr.split(",").map((elfString: string): ElfWithRange => {
+        const elfRanges: string[] = elfString.split("-");
+        return new ElfWithRange(Number.parseInt(elfRanges[0]), Number.parseInt(elfRanges[1]));
+      })).map((elfsWithRanges: ElfWithRange[]) => new ElfPair(elfsWithRanges[0], elfsWithRanges[1]))
+
+  }
+
 
 }
