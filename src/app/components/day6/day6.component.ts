54e42dc7f9be2dc8e0b89c4fb09abb973ff3e944
amoeller <alexander.moeller@adesso.de>
2022-12-06 16:21:57 +0100

    solved day6

--- src/app/components/day6/day6.component.ts
+++ src/app/components/day6/day6.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {DataService} from "../../services/data.service";
+
 
 @Component({
   selector: 'app-day6',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./day6.component.scss']
 })
 export class Day6Component implements OnInit {
+  loseung_1: number = 0;
+  loseung_2: number = 0;
 
-  constructor() { }
+  constructor(public dataService: DataService) {
+  }
 
   ngOnInit(): void {
+    this.dataService.getDataOfDay("6").subscribe(data => {
+      this.loseung_1 = this.findFirstPattern(data, 4);
+      this.loseung_2 = this.findFirstPattern(data, 14);
+    })
+  }
+
+  findFirstPattern(data: string, patternLength: number) {
+    let index = 0;
+    for (let i = patternLength - 1; i < data.length; i++) {
+      const uniqueSet = new Set()
+      for (let j = patternLength - 1; j >= 0; j--) {
+        uniqueSet.add(data[i - j]);
+      }
+      if (uniqueSet.size === patternLength) {
+        index = i;
+        break;
+      }
+    }
+    return index + 1;
   }
 
 }
